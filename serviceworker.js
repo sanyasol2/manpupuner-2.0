f265c879943372542e7532538183eb3bb2e58028
sanyasol2 <sanyasol706@gmail.com>
2026-07-16 16:12:53 +0300

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'digital-books-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/about.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    )
+  );
+});
